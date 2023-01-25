c612fcfe9d16f90af810be5b469598d5807be56f
October CMS <hello@octobercms.com>
2023-01-25 11:29:49 +1100

    Spawn plugin

---
+++ Plugin.php
@@ -0,0 +1,32 @@
+<?php namespace RainLab\Dusk;
+
+use System\Classes\PluginBase;
+
+/**
+ * Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * pluginDetails
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'October Dusk',
+            'description' => 'Browser testing for October CMS, powered by Laravel Dusk.',
+            'author' => 'Alexey Bobkov, Samuel Georges',
+            'icon' => 'icon-chrome',
+            'homepage' => 'https://github.com/rainlab/dusk-plugin',
+        ];
+    }
+
+    /**
+     * register the service provider
+     */
+    public function register()
+    {
+        $this->registerConsoleCommand('test.dusk', \October\Test\Console\Dusk::class);
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Dusk Plugin
+
+Browser testing for October CMS, powered by Laravel Dusk.
+
+## Installation Instructions
+
+To install with Composer, run from your project root
+
+```bash
+composer require rainlab/dusk-plugin
+```
+
+> **Warning**: This plugin is intended for local environments only. Do not install this plugin in any production environment, ever.
+
+### License
+
+This plugin is an official extension of the October CMS platform and is free to use if you have a platform license. See [EULA license](https://octobercms.com/eula) for more details.
